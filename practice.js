2bebcd19d2c06dc189dc5083d044f802276b0bc2
Jakub <flandak@gmail.com>
2022-10-22 00:18:19 +0200

    last commit of the day

--- practice.js
+++ practice.js
@@ -1,6 +1,21 @@
-const numbers = [1, 2, 3, 4, 5]; 
- 
-const bigNumbers = numbers.foreach(number => {
-  return number * 10;
-});
-console.log(bigNumbers)
+let array = ['JavaScript', 'Array', 'Reverse', 'Example'];
+let newArray = [];
+
+for(i = array.length-1; i >= 0; i--) {
+  newArray.push(array[i]);
+}
+
+console.log(newArray);
+
+
+const arr = ['sense.','make', 'all', 'will', 'This'];
+let reversed = [];
+const reverseArray = arr => {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    
+}
+// console.log(reversed)
+
+console.log(reverseArray(arr)); 
