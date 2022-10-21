a4362391466906f419cbaf77f20a6d049f62d48d
Jakub <flandak@gmail.com>
2022-10-21 11:09:31 +0200

    adding comments

--- rockPaperScissors/rock.js
+++ rockPaperScissors/rock.js
@@ -1,3 +1,6 @@
+// Rock paper scissors is a classic two player game. Each player chooses either rock, paper, or scissors. The items are compared, and whichever player chooses the more powerful item wins.
+
+
 const  getUserChoice = userInput =>{
     userInput = userInput.toLowerCase();
     if (userInput === 'rock' || userInput === 'paper'|| userInput === 'scissors' || userInput === 'bomb'){
