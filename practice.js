d22fbe23c62a9c67f93a0cfd5fb1fb0126096fde
Jakub <flandak@gmail.com>
2022-10-24 19:51:39 +0200

    classes exc-library

--- practice.js
+++ practice.js
@@ -1,21 +1,67 @@
-let array = ['JavaScript', 'Array', 'Reverse', 'Example'];
-let newArray = [];
+class Media {
+  constructor(title){    
+    this._title = title;    
+    this._isCheckedOut = false;   
+    this._ratings = [];
+  }
+  get title(){
+    return this._title;
+  }
+  get isCheckedOut(){
+    return this._isCheckedOut;
+  }
+  get ratings(){
+    return this._ratings;
+  }
+  set isCheckedOut(value){
+    this._isCheckedOut = value;
+  }
+ toggleCheckOutStatus(){      
+      this._isCheckedOut = !this._isCheckedOut;
+     
+  }
+  getAverageRating(){
+   let ratingsSum = this.ratings.reduce((accumulator, rating) => accumulator + rating);
+  return ratingsSum/ this.ratings.length;
+  }
 
-for(i = array.length-1; i >= 0; i--) {
-  newArray.push(array[i]);
+  addRating(ratings){
+    this._ratings.push(ratings);
+  }
 }
+class Book extends Media{
+  constructor(author, title, pages){
+    super(title);
+    this._author = author;   
+    this._pages = pages;
+  }
+  get author(){
+    return this._author;
+  }
+  get pages(){
+    return this._pages;
+  }
+  }
 
-console.log(newArray);
 
-
-const arr = ['sense.','make', 'all', 'will', 'This'];
-let reversed = [];
-const reverseArray = arr => {
-    for (let i = arr.length - 1; i >= 0; i--) {
-        reversed.push(arr[i]);
+  class Movie extends Media{
+    constructor(director, title, runTime){
+      super(title);
+      this._director = director;
+      this._runTime = runTime;
     }
-    
-}
-// console.log(reversed)
-
-console.log(reverseArray(arr)); 
+    get director(){
+      return this._director;
+    }
+    get runTime(){
+      return this._runTime;
+    }
+  }
+  const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544)
+historyOfEverything.toggleCheckOutStatus();
+// console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+// console.log(historyOfEverything.addRating());
+// console.log(historyOfEverything.addRating());
